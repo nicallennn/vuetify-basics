753fb6472426a818b46b452c86e825d36d79af39
Nick Allen <nick@scapplications.co.uk>
2018-12-04 21:50:04 +0000

    Font styles etc.

---
+++ src/views/Contact.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>This is the contact page</h1>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,28 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>This is the home page</h1>
+
+    <!-- indigo background -> lighten 3 yellow text -> darken text -->
+    <p
+      class="indigo lighten-3 yellow--text text-darken-4 px-3"
+    >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores nemo minima harum velit aperiam vitae blanditiis praesentium necessitatibus voluptatem recusandae, vero quas, doloribus at cumque.</p>
+
+    <hr class="my-3">
+
+    <h1 class="display-3">Display-3 on h1</h1>
+    <h1 class="display-2">Display-2 on h1</h1>
+    <h1 class="display-1">Display-1 on h1</h1>
+    <h3 class="display-1">Display-1 on h3 *** display utitlities match font size ***</h3>
+
+    <hr class="my-3">
+
+    <p class="headline">This is a headline</p>
+    <p class="subheading bold">This is a subheading</p>
+    <p class="caption">This is a caption (used under images etc.)</p>
+    <p class="subheading font-weight-bold">This is a bold subheading</p>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
+export default {};
 </script>
